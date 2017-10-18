6f5d34404642747acc48ee3e12b4b24649d999fd
James French <james.dylan.french@gmail.com>
2017-10-18 19:08:59 -0400

    Add GPS dialog layout, add text entry to GPS dialog
    Further changes to the GPS dialog are pending based on how it needs to integrate with Google Maps.

--- DronePath/app/src/main/java/com/dronepath/GPSLocationDialogFragment.java
+++ DronePath/app/src/main/java/com/dronepath/GPSLocationDialogFragment.java
@@ -5,9 +5,12 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.app.DialogFragment;
 import android.app.Dialog;
+import android.view.LayoutInflater;
 
 /**
  * Created by Dylan on 10/17/2017.
+ *
+ * Creates and manages the dialog for manual GPS Location input
  */
 
 public class GPSLocationDialogFragment extends DialogFragment {
@@ -16,17 +19,26 @@ public class GPSLocationDialogFragment extends DialogFragment {
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         // The Builder class provides a wrapper for easily modifying a Dialog (popup menu)
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        // The layout inflater converts an XML layout into view objects
+        LayoutInflater inflater = getActivity().getLayoutInflater();
 
-        builder.setMessage("Testing")   // Text in window
+        // Define the dialog
+        builder.setView(inflater.inflate(R.layout.gps_dialog, null))
                 // "Setter methods" are chained together to modify the dialog's characteristics
+                .setMessage("Manually input a GPS address")   // Text in window
                 .setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        // User hit OK
+                    public void onClick(DialogInterface dialog, int id) { // User hit OK
+                        /* TODO 3 possible actions
+                            1) Pass GPS address to a sanity checker, then to the Google Maps class
+                            2) Pass straight to GMaps class which will perform its own sanity checks
+                            3) Store GPS address as a global and call a GMaps routine to display
+                                the updated info?
+                         */
                     }
                 })
                 .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        // User hit Cancel
+                    public void onClick(DialogInterface dialog, int id) { // User hit Cancel
+                        // Close the dialog without modifying any variables
                     }
                 });
 
